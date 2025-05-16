812e8f2d9f4e6de93901f2ca2470248c8f660031
abhishek199677 <puttapoguabhishek1007@gmail.com>
2025-05-16 15:31:44 +0530

    modular code added

--- modular_code_train_cnn.py
+++ modular_code_train_cnn.py
@@ -8,16 +8,18 @@ IMG_SIZE = (128,128)
 BATCH_SIZE = 16
 
 # data preprocessing
+#both train_datagen and val_datagen will rescale the images from RGB to black and white
 train_datagen  = ImageDataGenerator(rescale= 1./255)
 val_datagen = ImageDataGenerator(rescale = 1./255)
 
-train_data = train_datagen.flow_from_directory(
+#reading the files from the image folder
+train_data = train_datagen.flow_from_directory(    #train_data
     'data/train',
     target_size =IMG_SIZE,
     batch_size = BATCH_SIZE,
     class_mode = 'categorical'
 )
-val_data = val_datagen.flow_from_directory(
+val_data = val_datagen.flow_from_directory(     #validation_data
     'data/val',
     target_size =IMG_SIZE,
     batch_size = BATCH_SIZE,
@@ -25,9 +27,9 @@ val_data = val_datagen.flow_from_directory(
 )
 
 
-num_class = len(train_data.class_indices) # number of classes 
+num_class = len(train_data.class_indices) # shows the total number of classes 
 
-# model architecture
+#buildimg the image model architecture
 model =models.Sequential([
     layers.Conv2D(32 ,(3,3),activation = 'relu' ,input_shape = (128,128,3) ,padding ="same"),
     layers.MaxPooling2D(2,2),
@@ -74,3 +76,5 @@ model.fit(
 model.save("image_cnn_classifier.h5")
 
 model.summary()
+
+9989253841...........! 
